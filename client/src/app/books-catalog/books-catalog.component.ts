39cbda9601533d108a705d81d83f5003b811c11a
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-11 21:57:43 +0300

    Create Breadcrumb

--- client/src/app/books-catalog/books-catalog.component.ts
+++ client/src/app/books-catalog/books-catalog.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FiltersBookComponent } from "./filters-book/filters-book.component";
 import { CatalogComponent } from "./catalog/catalog.component";
 import { PaginationComponent } from "./pagination/pagination.component";
+import { BreadcrumbComponent } from "../core/breadcrumb/breadcrumb.component";
 
 @Component({
   selector: 'app-books-catalog',
-  imports: [FiltersBookComponent, CatalogComponent, PaginationComponent],
+  imports: [FiltersBookComponent, CatalogComponent, PaginationComponent, BreadcrumbComponent],
   templateUrl: './books-catalog.component.html',
   styleUrl: './books-catalog.component.css',
 })
