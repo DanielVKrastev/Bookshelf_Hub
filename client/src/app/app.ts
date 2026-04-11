38b255c5019e95d71cbe649c5f729fd61dd1cfa7
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-11 21:18:06 +0300

    Create footer component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from "./core/header/header.component";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
