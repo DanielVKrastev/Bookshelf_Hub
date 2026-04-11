050b598fdd860975c38fc75ce34cb501a388c9a0
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-11 21:13:54 +0300

    Recently Added Books for Home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from "./hero-section/hero-section.component";
 import { TopRatingBooksComponent } from "./top-rating-books/top-rating-books.component";
+import { RecentlyAddedBooksComponent } from "./recently-added-books/recently-added-books.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroSectionComponent, TopRatingBooksComponent],
+  imports: [HeroSectionComponent, TopRatingBooksComponent, RecentlyAddedBooksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
---
+++ client/src/app/home/recently-added-books/recently-added-books.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recently-added-books',
+  imports: [],
+  templateUrl: './recently-added-books.component.html',
+  styleUrl: './recently-added-books.component.css',
+})
+export class RecentlyAddedBooksComponent {}
