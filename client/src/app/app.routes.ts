d719c704aec8a3e156d53c07f139136805f2cdda
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-11 22:11:37 +0300

    Create Register Page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BooksCatalogComponent } from './books-catalog/books-catalog.component';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -13,4 +14,5 @@ export const routes: Routes = [
     ] },
      // User routing
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
