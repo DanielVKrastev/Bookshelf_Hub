8619acc5aecf8d2101c46be252fdbe7656286346
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-11 21:02:34 +0300

    Create hero section component for Home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "./hero-section/hero-section.component";
+import { MostHighReviewBooksComponent } from "./most-high-review-books/most-high-review-books.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeroSectionComponent, MostHighReviewBooksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
