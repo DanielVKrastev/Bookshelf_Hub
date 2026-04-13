7b3d421d33ee7a956baea5d5c796268ea384e6fa
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-13 09:13:20 +0300

    Update design Top Rating Books component

--- client/src/app/home/top-rating-books/top-rating-books.component.ts
+++ client/src/app/home/top-rating-books/top-rating-books.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-top-rating-books',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './top-rating-books.component.html',
   styleUrl: './top-rating-books.component.css',
 })
