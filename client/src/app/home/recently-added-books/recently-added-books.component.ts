24e50b48e67c56dbb0ac1b8b02fe74800d1184ec
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-13 09:06:10 +0300

    Update design recently added book component

--- client/src/app/home/recently-added-books/recently-added-books.component.ts
+++ client/src/app/home/recently-added-books/recently-added-books.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-recently-added-books',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recently-added-books.component.html',
   styleUrl: './recently-added-books.component.css',
 })
