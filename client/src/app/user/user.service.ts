7269c7316055edc820754b1858e92a9b7e08c49a
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-14 22:35:34 +0300

    Create user.service.ts

---
+++ client/src/app/user/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { UserForAuth } from '../types/user';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private user$$ = new BehaviorSubject<UserForAuth | null>(null);
+  public user$ = this.user$$.asObservable();
+
+  USER_KEY = '[user]';
+  user: UserForAuth | null = null;
+  userSubscription: Subscription | null = null;
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor(private http: HttpClient){
+    this.userSubscription = this.user$.subscribe((user) => {
+      this.user = user;
+    })
+  }
+
+  login(email: string, password: string) {
+    return this.http
+            .post<UserForAuth>('/api/login', { email, password })
+            .pipe(tap((user) => this.user$$.next(user)));
+  }
+}
