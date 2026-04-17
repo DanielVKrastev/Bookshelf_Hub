7714f3d7185ae852200878a335462c07eee0143e
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-17 23:42:10 +0300

    Change when user is logged in

--- client/src/app/home/hero-section/hero-section.component.ts
+++ client/src/app/home/hero-section/hero-section.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-hero-section',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css',
 })
-export class HeroSectionComponent {}
+export class HeroSectionComponent {
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  constructor(private userService: UserService) { }
+}
