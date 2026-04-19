e51333cf57df65983340f569f8fe129dadb59fd8
Daniel Krastev <danielkrastev01@gmail.com>
2026-04-19 14:20:43 +0300

    TODO: Create pagination functionality

--- client/src/app/books/books-catalog/pagination/pagination.component.ts
+++ client/src/app/books/books-catalog/pagination/pagination.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.css',
 })
